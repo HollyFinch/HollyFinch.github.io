ad6ccb5b863946698b91ada44e636ea1e4034029
Holly Finch <49234526+HollyFinch@users.noreply.github.com>
2024-02-07 22:56:59 +0000

    Add files via upload

--- hello.js
+++ hello.js
@@ -2,18 +2,17 @@ var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 
-if (hourNow > 17 && hourNow <= 21) {
-  greeting = "Fine evening";
-} else if (hourNow > 11) {
-  greeting = "Good afternoon";
-} else if (hourNow > 3) {
-  greeting = "Top of the morning to you 🎩☕";
-} else if (hourNow == 0) {
+if (hourNow == 0) {
   greeting = "Happy midnight ⛤🕯️👻";
-} else if (hourNow > 21) {
-  greeting = "Bonne nuit";
+} else if (hourNow > 3 && hourNow <= 11) {
+  greeting = "Top of the morning to you 🎩☕";
+} else if (hourNow > 11 && hourNow <= 17) {
+  greeting = "Good afternoon";
+} else if (hourNow > 17 && hourNow <= 21) {
+  greeting = "Fine evening";
 } else {
-  greeting = "Welcome";
+  greeting = "Bonne nuit";
 }
 
 document.write(greeting);
+console.log(hourNow);
